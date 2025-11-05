c234b02138e1c7d2981503e9dec57ad0464b4a2c
Rafi <mohamedrafikhan2009@gmail.com>
2025-11-05 14:00:27 +0000

    I forgot

--- src/vlc/VLCBitmap.hx
+++ src/vlc/VLCBitmap.hx
@@ -1,7 +1,7 @@
 package vlc;
 
-#if !(desktop || android)
-#error "The current target platform isn't supported by hxCodec. If you're targeting Windows/Mac/Linux/Android and getting this message, please contact us.";
+#if !(desktop || mobile)
+#error "The current target platform isn't supported by hxCodec. If you're targeting Windows/Mac/Linux/Android/iOS and getting this message, please contact us.";
 #end
 import cpp.NativeArray;
 import cpp.Pointer;
